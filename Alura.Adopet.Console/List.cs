264943fc73b31c1a744f04447d6d5dd4de54d710
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-07-23 19:23:22 -0300

    refactor separated functions

---
+++ Alura.Adopet.Console/List.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Adopet.Console
+{
+    internal class List
+    {
+        HttpClient client;
+        public List()
+        {
+            client = new HttpClient("http://localhost:5057");
+        }
+        public async Task ListaDadosPetsDaAPIAsync()
+        {
+            IEnumerable<Pet>? pets = await ListaPetsAsync();
+            System.Console.WriteLine("----- Lista de Pets importados no sistema -----");
+            foreach (var pet in pets)
+            {
+                System.Console.WriteLine(pet);
+            }
+        }
+
+        HttpClient ConfiguraHttpClient(string url)
+        {
+            HttpClient _client = new HttpClient();
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.BaseAddress = new Uri(url);
+            return _client;
+        }
+
+        async Task<IEnumerable<Pet>?> ListaPetsAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync("pet/list");
+            return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
+        }
+    }
+}
